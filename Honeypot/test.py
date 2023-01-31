7015054f758c750701e0bf9374ed4526713908cb
Mr-B3A5T <vishwa.i@olak.org>
2023-01-31 13:54:32 +0530

    Update test.py

--- Honeypot/test.py
+++ Honeypot/test.py
@@ -1,91 +1,61 @@
-import socket
+from flask import Flask, Response
 import cv2
-import numpy as np
-import tkinter as tk
-from tkinter import messagebox
+import socket
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+app = Flask(__name__)
+
+# Load the video file
+video = cv2.VideoCapture("footage.mp4")
+
+# Create a TCP/IP socket for the honeypot
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to a specific address and port
-server_address = ('0.0.0.0', 8000)
-sock.bind(server_address)
+server_address = ('0.0.0.0', 12345)
+server_socket.bind(server_address)
 
 # Listen for incoming connections
-sock.listen(1)
+server_socket.listen(1)
 
-# Load a sample video file
-cap = cv2.VideoCapture('sample_video.mp4')
-
-# Define the login credentials
-username = "admin"
-password = "secret"
-
-# Create a Tkinter window for the dashboard
-root = tk.Tk()
-root.title("CCTV System Dashboard")
-
-def login():
-    # Get the entered username and password
-    entered_username = username_entry.get()
-    entered_password = password_entry.get()
-
-    # Check if the entered credentials are correct
-    if entered_username == username and entered_password == password:
-        # Show the video feed
-        frame.pack()
-        root.after(30, update_frame)
-    else:
-        # Show an error message
-        messagebox.showerror("Error", "Invalid username or password")
-
-# Add a label and entry for the username
-username_label = tk.Label(root, text="Username:")
-username_label.pack()
-username_entry = tk.Entry(root)
-username_entry.pack()
-
-# Add a label and entry for the password
-password_label = tk.Label(root, text="Password:")
-password_label.pack()
-password_entry = tk.Entry(root, show="*")
-password_entry.pack()
-
-# Add a button to log in
-login_button = tk.Button(root, text="Login", command=login)
-login_button.pack()
-
-# Add a label to display the video feed
-frame = tk.Label(root, text="CCTV Feed")
-
-def update_frame():
-    ret, img = cap.read()
-    if ret:
-        # Convert the frame to a photo image
-        img = cv2.resize(img, (640, 480))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(img)
-        img = ImageTk.PhotoImage(img)
-
-        # Update the label with the new image
-        frame.configure(image=img)
-        frame.image = img
-
-    # Schedule the next update
-    root.after(30, update_frame)
-
-def start_server():
+def generate_frames():
+    # Continuously accept incoming connections and log them
     while True:
-        # Wait for a connection
-        print('waiting for a connection')
-        connection, client_address = sock.accept()
-
+        print('Waiting for a connection...')
+        connection, client_address = server_socket.accept()
         try:
-            print('connection from', client_address)
+            print('Accepted connection from', client_address)
+            while True:
+                # Read a frame from the video
+                ret, frame = video.read()
 
-            # Send the fake CCTV footage
-            while cap.isOpened():
-                ret, frame = cap.read()
-                if ret:
-                    # Convert the frame to a byte array
-                    frame = cv2.resize(frame, (640, 480
+                # If the video has ended, restart it
+                if not ret:
+                    video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                    ret, frame = video.read()
+
+                # Encode the frame to a JPEG image and send it to the client
+                jpeg_frame = cv2.imencode('.jpg', frame)[1].tobytes()
+                connection.sendall(jpeg_frame)
+        finally:
+            connection.close()
+
+@app.route('/')
+def index():
+    return """
+    <html>
+    <head>
+        <title>CCTV Honeypot</title>
+    </head>
+    <body>
+        <img src="http://localhost:12345/video_feed"/>
+    </body>
+    </html>
+    """
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
